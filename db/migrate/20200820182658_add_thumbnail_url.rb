8796ec629e6e584334c68ed55e558dfce33f43fd
sulizbasnet <sulizzbasnet@gmail.com>
2020-08-28 10:27:55 -0700

    deleted the bug table

--- db/migrate/20200820182658_add_thumbnail_url.rb
+++
@@ -1,6 +0,0 @@
-class AddThumbnailUrl < ActiveRecord::Migration[5.2]
-  def change
-
-    add_column :videos, :thumbnail_url, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -73,11 +73,12 @@ ActiveRecord::Schema.define(version: 2020_08_26_065732) do
   create_table "videos", force: :cascade do |t|
     t.string "title", null: false
     t.string "description", null: false
+    t.string "thumbnail_url"
     t.integer "views", null: false
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "thumbnail_url"
+    t.index ["user_id"], name: "index_videos_on_user_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
