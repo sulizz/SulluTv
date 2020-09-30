f9066893a9096aad5069a09338dc56b43fb89112
sulizbasnet <sulizzbasnet@gmail.com>
2020-09-30 15:37:44 -0700

    fixed index css

--- frontend/components/Video/video_index/video_index_item.jsx
+++ frontend/components/Video/video_index/video_index_item.jsx
@@ -11,14 +11,15 @@ const VideoIndexItem = ({ video }) => {
                     />
                 </Link>
 
+                <h1 className ='avatar'>{video.username[0].toUpperCase()}</h1>
                 <div className= 'video-bottom-text'>
-                    <h1 className ='avatar'>{video.username[0]}</h1>
 
                     <div className='video-text'>
                         <h2> {video.title} </h2>
                         <h1> {video.username}</h1>
                         <h1>{video.views}  views</h1>
                     </div>
+                <div className='empty'>   </div>
                 </div>
             </div>
         
